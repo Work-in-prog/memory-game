908f867cf68d3c2f027b9e03287c9d098c8e79a9
khoury smith <khourysmith@khourys-MacBook-Pro.local>
2020-02-01 18:29:33 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-console. log("Up and Running");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console. log("User flipped" + cardOne);
+const cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push("cardOne");
+console.log("User flipped queen");
+const cardTwo = cards[2];
+cardsInPlay.push("cardTwo");
+console.log("User flipped king")
+if (cardsInPlay.length === 3){
+	console.log(cardsInPlay.length);
+} else if (cardsInPlay[0] === cardsInPlay[0]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
